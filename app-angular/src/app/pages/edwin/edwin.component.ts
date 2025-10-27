0f8e63fcc692a9a44c45f39276bbc4e6709a9bc8
edwin quiroz <quiroz_020927@hotmail.com>
2025-10-27 17:14:48 -0600

    Este commit es para editar mi pagina personal.

--- app-angular/src/app/pages/edwin/edwin.component.ts
+++ app-angular/src/app/pages/edwin/edwin.component.ts
@@ -2,10 +2,29 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-edwin',
-  imports: [],
   templateUrl: './edwin.component.html',
-  styleUrl: './edwin.component.scss'
+  styleUrls: ['./edwin.component.scss']
 })
 export class EdwinComponent {
+  onMouseMove(event: MouseEvent) {
+    const tarjeta = event.currentTarget as HTMLElement;
+    const rect = tarjeta.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
 
+    tarjeta.style.setProperty('--x', `${x}px`);
+    tarjeta.style.setProperty('--y', `${y}px`);
+
+    const rotateX = (y / rect.height - 0.5) * 20;
+    const rotateY = (x / rect.width - 0.5) * -20;
+
+    tarjeta.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+  }
+
+  onMouseLeave() {
+    const tarjetas = document.querySelectorAll('.tarjeta');
+    tarjetas.forEach(tarjeta => {
+      (tarjeta as HTMLElement).style.transform = 'rotateX(0deg) rotateY(0deg)';
+    });
+  }
 }
